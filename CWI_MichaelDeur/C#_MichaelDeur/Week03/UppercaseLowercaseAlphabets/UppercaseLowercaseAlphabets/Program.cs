6656e56f139b47a837899652039b56bb6631bc57
michaeldeur <91447929+michaeldeur@users.noreply.github.com>
2025-02-28 00:27:25 -0700

    Add files via upload

---
+++ CWI_MichaelDeur/C#_MichaelDeur/Week03/UppercaseLowercaseAlphabets/UppercaseLowercaseAlphabets/Program.cs
@@ -0,0 +1,37 @@
+﻿//Michael Deur
+//September 8, 2023
+
+using System;
+using static System.Console;
+
+class Program
+{
+    static void Main()
+    {
+        String chInput = ""; //Variables
+        String output = "";
+        char ch;
+
+        WriteLine("Enter a character: "); //Get input
+        chInput = ReadLine();
+        ch = Convert.ToChar(chInput);
+
+        bool uppercase = (ch >= 'A' && ch <= 'Z'); //Booleans
+        bool lowercase = (ch >= 'a' && ch <= 'z');
+
+        if(uppercase) //Check for uppercase, lowercase, or neither
+        {
+            output = "Uppercase";
+
+        } else if(lowercase)
+        {
+            output = "Lowercase";
+
+        } else
+        {
+            output = "Not an alphabetic character";
+        }
+
+        WriteLine(output); //Display output 
+    }
+}
