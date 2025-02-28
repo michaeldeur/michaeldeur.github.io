65d6559184b1d6eea29be2f7e6c979755c9f32cd
michaeldeur <91447929+michaeldeur@users.noreply.github.com>
2025-02-28 00:37:16 -0700

    Add files via upload

---
+++ CWI_MichaelDeur/C#_MichaelDeur/Week08/TeamProject01/LargestNumber/LargestNumber/Program.cs
@@ -0,0 +1,46 @@
+﻿//Team Project
+//October 09, 2023
+
+using System;
+using static System.Console;
+
+class LargestNumber { 
+    private static void PrintArray(int[] array){
+        Write("Array Values:");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Write("  {0}",array[i]);
+        }
+        WriteLine();
+    }
+    private static void SortArray(int[] array) {
+        Array.Sort(array);
+    }
+    private static void PrintLargestValue(int[] array)
+    {
+        Write("{0}  ", array[array.Length-1]);
+    }
+
+    static void Main()
+    {
+        int[] firstArray = {4, 2, 7, 1};
+        int[] secondArray = {20, 70, 40, 90};
+        int[] thirdArray = {1, 2, 0};
+
+        PrintArray(firstArray);
+        PrintArray(secondArray);
+        PrintArray(thirdArray);
+
+        WriteLine();
+
+        SortArray(firstArray);
+        SortArray(secondArray);
+        SortArray(thirdArray);
+
+        WriteLine("The largest in each array are: ");
+
+        PrintLargestValue(firstArray);
+        PrintLargestValue(secondArray);
+        PrintLargestValue(thirdArray);  
+    }
+}
---
+++ CWI_MichaelDeur/C#_MichaelDeur/Week08/TeamProject01/RepeatStringMod01/RepeatStringMod01/Program.cs
@@ -0,0 +1,34 @@
+﻿//Team
+//October 09, 2023
+
+using System;
+using static System.Console;
+
+class Program
+{
+    private static void GetStringRepeatNum(out string myString, out int repeatTimes)
+    {
+        //Get string to repeat
+        Write("Enter the string to repeat: ");
+        myString = ReadLine();
+
+        WriteLine();
+
+        //Get repeat times
+        Write("\nEnter how many times to repeat the string: ");
+        repeatTimes = Convert.ToInt32(ReadLine());
+    }
+
+    private static void PrintString(string str, int repeatTimes)
+    {
+        for (int i = 0; i < repeatTimes; i++)
+        {
+            WriteLine(str);
+        }
+    }
+    static void Main(string[] args)
+    {
+        GetStringRepeatNum(out string enterString, out int repeatNum);
+        PrintString(enterString, repeatNum);
+    }//end Main
+}
