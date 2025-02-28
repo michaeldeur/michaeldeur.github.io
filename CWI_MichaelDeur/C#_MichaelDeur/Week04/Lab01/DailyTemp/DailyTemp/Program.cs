9a3c5dde373c3c3c2fb4279dcf78d45852225543
michaeldeur <91447929+michaeldeur@users.noreply.github.com>
2025-02-28 00:28:34 -0700

    Add files via upload

---
+++ CWI_MichaelDeur/C#_MichaelDeur/Week04/Lab01/DailyTemp/DailyTemp/Program.cs
@@ -0,0 +1,56 @@
+﻿//Michael Deur
+//September 13, 2023
+
+using System;
+using static System.Console;
+
+class Program
+{
+    static void Main()
+    {
+        string tempInput = "";
+        double tempNum = 0;
+        double tempTot = 0;
+        double avgTemp = 0;
+        int inc = 0;
+
+
+
+
+        WriteLine("Enter a daily temperature: ");
+        tempInput = ReadLine();
+        tempNum = Convert.ToDouble(tempInput);
+
+        if (tempNum > -20 && tempNum < 130)
+        {
+            tempTot += tempNum;
+            inc++;
+        } else
+        {
+            WriteLine("  * Invalid Temperature - Must be between -20 and 130 *");
+        }
+
+        while (tempNum != 999)
+        {
+            WriteLine("Enter a daily temperature or 999 to exit: ");
+            tempInput = ReadLine();
+            tempNum = Convert.ToDouble(tempInput);
+
+            if (tempNum > -20 && tempNum < 130)
+            {
+                tempTot += tempNum;
+                inc++;
+            }
+            else if (tempNum == 999)
+            {
+                break;
+            } else
+            {
+                WriteLine("  * Invalid Temperature - Must be between -20 and 130 *");
+            }
+        }
+
+        avgTemp = tempTot / inc;
+        WriteLine("The average daily temperature is: {0}", avgTemp.ToString("F2"));
+    }
+}
---
+++ CWI_MichaelDeur/C#_MichaelDeur/Week04/Lab01/TempConversion/TempConversion/Program.cs
@@ -0,0 +1,135 @@
+﻿//Michael Deur
+//September 15, 2023
+
+using System;
+using static System.Console;
+
+class Program
+{
+    static void Main()
+    {
+        /* string conDec = "";
+         double tempInput = 0;
+         double tempOutput = 0;
+
+         Write("Press c to convert temperature from Fahrenheit to Celsius\nor\nPress f to convert temperature from Celsius to Fahrenheit\nEnter your choice (c or f): ");
+         conDec = (ReadLine());
+
+         if (conDec == "F" || conDec == "f") {
+             Write("\nEnter temperature in Celsius: ");
+             tempInput = Convert.ToDouble(ReadLine());
+             tempOutput = (tempInput * 1.8) + 32;
+             WriteLine("\nTemperature in Fahrenheit: {0}", tempOutput);
+         } else if (conDec == "C" || conDec == "c") {
+             Write("\nEnter temperature in Fahrenheit: ");
+             tempInput = Convert.ToDouble(ReadLine());
+             tempOutput = (tempInput - 32) / 1.8;
+             WriteLine("\nTemperature in Fahrenheit: {0}", tempOutput);
+         }
+         else
+         {
+             WriteLine("\nInvalid Choice !!!");
+         }*/
+
+        double dSaleDbl = 0;
+        double eSaleDbl = 0;
+        double fSaleDbl = 0;
+        double saleDbl = 0;
+        string initialInput = "";
+        double totSale = 0;
+
+        Write("Enter a salesperson initial >> ");
+        initialInput = String.ToUpper(ReadLine());
+        if (initialInput == "D" || initialInput == "E" || initialInput == "F") {
+            Write("Enter amount of sale >> ");
+            saleDbl = Convert.ToDouble(ReadLine());
+
+            if (initialInput == "D")
+            {
+                dSaleDbl += dSaleDbl;
+            }
+            else if (initialInput == "E")
+            {
+                eSaleDbl += eSaleDbl;
+            }
+            else if (initialInput == "F")
+            {
+                fSaleDbl += fSaleDbl;
+            }
+            else
+            {
+                WriteLine("  ** Invalid Salesperson");
+            }
+        }
+        else
+        {
+            WriteLine("  ** Invalid Salesperson");
+        }
+        while (initialInput == "Z")
+            {
+                Write("Enter next salesperson initial >> ");
+                initialInput = String.ToUpper(ReadLine());
+                if (initialInput == "Z")
+                {
+                    break;
+                }
+                else
+                {
+                    WriteLine("  ** Invalid Salesperson");
+                    continue;
+                }
+
+                Write("Enter amount of sale >> ");
+                saleDbl = Convert.ToDouble(ReadLine());
+
+                if (initialInput == "D")
+                {
+                    dSaleDbl += dSaleDbl;
+                }
+                else if (initialInput == "E")
+                {
+                    eSaleDbl += eSaleDbl;
+                }
+                else if (initialInput == "F")
+                {
+                    fSaleDbl += fSaleDbl;
+                }
+                else
+                {
+                    WriteLine("  ** Invalid Salesperson");
+                    continue;
+                }
+              
+            }
+
+        WriteLine("Danielle sold {0} {0,27}", dSaleDbl.ToString("c"));
+        WriteLine("Edward sold {0} {0,27}", dSaleDbl.ToString("c"));
+        WriteLine("Francis sold {0} {0,27}", dSaleDbl.ToString("c"));
+
+        totSale = dSaleDbl + eSaleDbl+ fSaleDbl;
+
+        WriteLine("Total sales were {0}", totSale.ToString("c"));
+
+        if (dSaleDbl > eSaleDbl && dSaleDbl > fSaleDbl)
+        {
+            WriteLine("Danielle sold the most");
+        } else if (dSaleDbl < eSaleDbl && fSaleDbl < eSaleDbl)
+        {
+            WriteLine("Edward sold the most");
+        } else if (fSaleDbl > dSaleDbl && fSaleDbl > eSaleDbl)
+        {
+            WriteLine("Francis sold the most");
+        } else if (dSaleDbl == eSaleDbl && eSaleDbl == fSaleDbl) {
+            WriteLine("Everyone sold the same amount");
+        } else if (eSaleDbl == fSaleDbl && eSaleDbl > dSaleDbl)
+        {
+            WriteLine("Edward and Francis sold the most");
+        } else if (eSaleDbl == dSaleDbl && eSaleDbl > fSaleDbl)
+        {
+            WriteLine("Edward and Danielle sold the most");
+        } else if (dSaleDbl ==  fSaleDbl && fSaleDbl > eSaleDbl)
+        {
+            WriteLine("Danielle and Francis sold the most");
+        }
+    }
+}
