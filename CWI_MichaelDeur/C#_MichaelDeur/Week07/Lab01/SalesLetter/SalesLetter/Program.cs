b7b2bf391d5211d1eda6ff230bf49e6cdac5eee2
michaeldeur <91447929+michaeldeur@users.noreply.github.com>
2025-02-28 00:35:40 -0700

    Add files via upload

---
+++ CWI_MichaelDeur/C#_MichaelDeur/Week07/Lab01/SalesLetter/SalesLetter/Program.cs
@@ -0,0 +1,27 @@
+﻿//Michael Deur
+//October 2, 2023
+
+using System;
+using static System.Console;
+
+class Program
+{
+    private static void DisplayContactInfo()
+    {
+        WriteLine("C# Company\r\nPhone: 555-1234     Cell: 555-0912\r\nEmail: csharpcompany@csharp.com\r\nOn the Web at www.csharpcompanyforyou.com");
+    }
+
+    static void Main(string[] args)
+    {
+        WriteLine("From");
+
+        DisplayContactInfo();
+
+        WriteLine("-------------------------");
+        WriteLine("Dear Client\r\nWe want to provide you good service.\r\nFeel free to contact us at any time.\r\nLooking forward to a long relationship.");
+        WriteLine("-------------------------");
+
+        DisplayContactInfo();
+
+    }
+}
---
+++ CWI_MichaelDeur/C#_MichaelDeur/Week07/Lab02/PaintingEstimate/PaintingEstimate/Program.cs
@@ -0,0 +1,35 @@
+﻿//Michael Deur
+//October 02, 2023
+
+using System;
+using System.Diagnostics;
+using static System.Console;
+
+class Program
+{
+    private static double estimatePaintCost(double width, double length)
+    {
+        double cost = 0.0;
+        const double HEIGHT = 9;
+        const double PRICE = 6;
+
+        cost = ((length * HEIGHT * 2) + (width * HEIGHT * 2)) * PRICE;
+        return cost;
+    }
+    static void Main(string[] args)
+    {
+        double roomLength = 0;
+        double roomWidth = 0;
+        double Price = 0;
+
+        WriteLine("Enter the length of the room in feet:");
+        roomLength = Convert.ToDouble(Console.ReadLine());
+
+        WriteLine("Enter width of room in feet:");
+        roomWidth = Convert.ToDouble(Console.ReadLine());
+
+        Price = estimatePaintCost(roomWidth, roomLength);
+
+        WriteLine("Cost of job for {0} x {1} foot room is {2}", roomLength, roomWidth, Price.ToString("C"));
+    }
+}
