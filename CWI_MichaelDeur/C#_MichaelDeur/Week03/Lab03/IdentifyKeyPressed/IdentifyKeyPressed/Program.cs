f49278c59e685c29eaef8ee0462ebded7ef5e1a7
michaeldeur <91447929+michaeldeur@users.noreply.github.com>
2025-02-28 00:23:34 -0700

    Add files via upload

---
+++ CWI_MichaelDeur/C#_MichaelDeur/Week03/Lab03/IdentifyKeyPressed/IdentifyKeyPressed/Program.cs
@@ -0,0 +1,30 @@
+﻿//Michael Deur
+//September 6, 2023
+using System;
+using static System.Console;
+
+class Program
+{
+    static void Main()
+    {
+        string inputChar = "";
+        char testChar = '\0';
+
+
+        Write("Hit any key on the keyboard and learn what type of character it is: ");
+        inputChar = ReadLine();
+        testChar = char.Parse(inputChar);
+
+        if ((testChar >= '0') && (testChar <= '9'))
+        {
+            WriteLine("     {0} is a number", testChar);
+        } else if ((testChar >= 'A') && (testChar <= 'Z') || (testChar >= 'a') && (testChar <= 'z'))
+        {
+            WriteLine("     {0} is an alphabetical character", testChar);
+        } else
+        {
+            WriteLine("     {0} is a Special character", testChar);
+        }
+
+    }
+}
---
+++ CWI_MichaelDeur/C#_MichaelDeur/Week03/Lab04/Admission/Admission/Program.cs
@@ -0,0 +1,49 @@
+﻿//Michael Deur
+//September 10, 2023
+
+using System;
+using static System.Console;
+
+class Program
+{
+    static void Main()
+    {
+        string gpaInput = ""; //Variables
+        string gradeInput = "";
+        double gpaNum = 0;
+        double gradeNum = 0;
+        string output = "";
+
+        Write("Please enter your GPA: "); //Gets user input
+        gpaInput = ReadLine();
+        gpaNum = Convert.ToDouble(gpaInput);
+
+        Write("Please enter your test score: ");
+        gradeInput = ReadLine();
+        gradeNum = Convert.ToDouble(gradeInput);
+
+        if(gpaNum >= 3.0) //Calculates input
+        {
+            if(gradeNum >= 60)
+            {
+                output = "You are accepted";
+            } else
+            {
+                output = "You are rejected";
+            }
+
+        } else
+        {
+            if(gradeNum >= 80)
+            {
+                output = "You are accepted";
+            } else
+            {
+                output = "You are rejected";
+            }
+
+        }
+
+        WriteLine(output); //Displays output
+    }
+}
